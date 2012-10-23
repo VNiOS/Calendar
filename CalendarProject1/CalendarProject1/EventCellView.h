1b0769fc840a2eabc059b788d28f126f91d2dd4e
tran minh nghia <nghiamtran@gmail.com>
2012-10-23 15:41:29 +0700

    insert EventCellView and EventViewControl

---
+++ CalendarProject1/CalendarProject1/EventCellView.h
@@ -0,0 +1,13 @@
+//
+//  EventCellView.h
+//  CalendarProject1
+//
+//  Created by Applehouse on 10/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventCellView : UITableViewCell
+
+@end
---
+++ CalendarProject1/CalendarProject1/EventViewControl.h
@@ -0,0 +1,13 @@
+//
+//  EventViewControl.h
+//  CalendarProject1
+//
+//  Created by Applehouse on 10/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventViewControl : UIViewController
+
+@end
